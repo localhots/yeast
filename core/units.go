6c8fd03a695f43e310e929699e04156e00ff9a9a
Gregory Eremin <g@erem.in>
2014-08-30 20:53:41 +0700

    Input outside of a unit

--- core/units.go
+++ core/units.go
@@ -3,7 +3,6 @@ package core
 import (
 	"code.google.com/p/go.net/context"
 	"github.com/localhots/yeast/units/aggregator"
-	"github.com/localhots/yeast/units/input"
 	"github.com/localhots/yeast/units/logger"
 	"github.com/localhots/yeast/units/power"
 	"github.com/localhots/yeast/units/sleep"
@@ -16,13 +15,12 @@ type (
 
 var (
 	Units = UnitsDict{
-		"aggregator":      aggregator.Call,
-		"logger":          logger.Call,
-		"power2":          power.Power2,
-		"power3":          power.Power3,
-		"power5":          power.Power5,
-		"input_from_flag": input.FromFlag,
-		"sleep":           sleep.Call,
-		"uuid":            uuid.Call,
+		"aggregator": aggregator.Call,
+		"logger":     logger.Call,
+		"power2":     power.Power2,
+		"power3":     power.Power3,
+		"power5":     power.Power5,
+		"sleep":      sleep.Call,
+		"uuid":       uuid.Call,
 	}
 )
