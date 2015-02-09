fb5cc8537537dac15a9c88292b10c03b51ef19c9
Gregory Eremin <g@erem.in>
2015-02-10 02:47:52 +0700

    Remove tools

--- core/chain_processor.go
+++ core/chain_processor.go
@@ -123,3 +123,20 @@ func readChains() map[string]interface{} {
 
 	return chains
 }
+
+func SyncronizeParallelChain(ctx context.Context, fn func(context.Context)) {
+	var (
+		pcontexts = ctx.Value("parallel_contexts").(chan context.Context)
+		pfinished = ctx.Value("parallel_finished").(chan struct{})
+		working   = true
+	)
+
+	for len(pcontexts) > 0 || working {
+		select {
+		case pctx := <-pcontexts:
+			fn(pctx)
+		case <-pfinished:
+			working = false
+		}
+	}
+}
